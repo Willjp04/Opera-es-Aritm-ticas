0d51590a9b5f1ad7213b03dbfa16ef5c46298185
Willian Sostenes <sostenes655@gmail.com>
2020-09-04 00:28:23 -0300

    Inicio projeto

---
+++ Operações Aritmeticas/src/operações/aritmeticas/OperaçõesAritmeticas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operações.aritmeticas;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class OperaçõesAritmeticas {
+
+   
+    public static void main(String[] args) {
+        
+    int n1 = 5;
+    int n2 = 6;
+    float m = (n1+n2) /2;
+        System.out.println("A média é " + (m));
+    }
+    
+}
